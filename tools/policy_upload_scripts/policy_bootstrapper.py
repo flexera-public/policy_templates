8461b68635860342111b3f0e48c97c67233073c1
MitchellGerdisch <mitchell.gerdisch@rightscale.com>
2018-06-28 08:38:25 -0500

    Policy bootstrap script enhancements and fixes (#68)
    * Modified to remove old applied synch policy template and re upload and
    apply it to force a resynch.
    Also removed force-upgrade option since it's not really applicable to
    this use-case.
    
    * Added support for specifying target accounts.
    
    * Updated changelog
    
    * Tweaked readme to mention password prompt.

--- tools/policy_upload_scripts/policy_bootstrapper.py
+++ tools/policy_upload_scripts/policy_bootstrapper.py
@@ -17,7 +17,7 @@ parser.add_argument('-o', '--org', required=True)
 parser.add_argument('-r', '--refresh_token', help='RightScale Refresh Token with Enterprise_Manager role. Not used if using username/password authentication.')
 parser.add_argument('-u', '--username', help='RightScale username with Enterprise_Manager role. Not used if using refresh_token authentication. Will prompt for password.')
 parser.add_argument('-a', '--account', help='Only needed if using username/password authentication.')
-parser.add_argument('-f', '--force_upgrade', default=1, help='OPTIONAL. Default: Template synch policy WILL overwrite templates with newer versions as they become available. Used by template synch policy. 1 => new versions of policies will overwrite previous version. 0 => do not overwrite.')
+parser.add_argument('-t', '--target_accounts', help='OPTIONAL: Comma-separated list of accounts in the specified organization to which the policy should be uploaded. Otherwise, policy is uploaded to all accounts in the org.')
 args = parser.parse_args()
 
 # Gather up the inputs
@@ -25,7 +25,13 @@ org = args.org
 refresh_token = args.refresh_token
 username = args.username
 account_id = args.account
-force_upgrade = args.force_upgrade
+target_accounts = args.target_accounts
+if (target_accounts):
+    target_accounts = target_accounts.split(",")
+
+# Synch Policy Template info used below
+policy_synch_policy_name = "Policy Template Synchronization Policy Template"
+policy_synch_template_uri = "/tools/policy_sync/policy_sync.pt"
 
 if refresh_token is None:
     if username is None:
@@ -68,8 +74,7 @@ accounts_json = json.loads(accounts.text)
 accounts_json.sort(key=lambda e: e["legacy"]["account_url"]) 
     
 ### Get the policy template from github
-pol_template_uri = "/tools/policy_sync/policy_sync.pt"
-pol_template_url = git_base_url + pol_template_uri
+pol_template_url = git_base_url + policy_synch_template_uri
 git_file = requests.get(pol_template_url)
 git_file = git_file.text
 
@@ -81,7 +86,7 @@ gov_headers = {
 }
 
 # get file name from policy template uri path
-filename =  pol_template_uri.split("/")[-1]
+filename =  policy_synch_template_uri.split("/")[-1]
 upload_body = {
 	"filename" : filename,
 	"source" : git_file
@@ -108,7 +113,7 @@ apply_body = {
         },
         {
             "name" : "force_upgrade",
-            "value" : force_upgrade
+            "value" : 0  
         },
     ]
 } 
@@ -118,6 +123,12 @@ for account in accounts_json:
     account_id = account["id"]
     account_name = account["name"]
     account_shard = rs_account_info.rs_account_shard(account)
+    
+    # Check if the target accounts were specified and if not, then skip this org's account
+    if ((target_accounts) and (str(account_id) not in target_accounts)):
+        continue
+        
+    # If we get here, then it's an account we need to process.
     print "####### Account, {} ({}) #######".format(account_name, account_id)
     if (shard != account_shard):
         if using_basic_auth:
@@ -142,7 +153,7 @@ for account in accounts_json:
                 },
                 {
                     "name" : "force_upgrade",
-                    "value" : force_upgrade
+                    "value" : 0
                 }
             ]
             apply_body["options"] = options
@@ -154,17 +165,59 @@ for account in accounts_json:
             print ""
             did_not_complete = True
             continue
+
+    gov_endpoint = "https://governance-{}.rightscale.com".format(shard)
+    gov_base_url = "{}/api/governance/projects/{}".format(gov_endpoint,account_id)
+    
+    # This script will always push the policy synch policy. 
+    # So if the policy synch policy is already in the account, terminate it if applied and remove it first. 
+    print "Removing old Policy Synchronization Policy from account, {}, if found.".format(account_id)
+    # See if the synch policy has been applied and if so, terminate it (them)
+    applied_policies = requests.get(gov_base_url+"/applied_policies", headers=gov_headers) 
+    applied_policies = json.loads(applied_policies.text)
+    try:
+        for applied_policy in applied_policies["items"]:
+            try: 
+                if (applied_policy["policy_template"]["name"] == policy_synch_policy_name):
+                    pol_href = applied_policy["href"]
+                    # terminate applied policy
+                    print "Terminating applied policy, {} (href: {})".format(applied_policy["policy_template"]["name"],pol_href)
+                    term_resp = requests.delete(gov_endpoint+pol_href, headers=gov_headers)
+            except:
+                print "Problem terminating policy"
+    except:
+        # no-op
+        print "No applied policies found."
+
+    # See if the synch policy has been uploaded and if so remove it - clean slate, baby
+    uploaded_policies = requests.get(gov_base_url+"/policy_templates", headers=gov_headers) 
+    uploaded_policies = json.loads(uploaded_policies.text)
+    try:
+        for uploaded_policy in uploaded_policies["items"]:
+            try:
+                if (uploaded_policy["name"] == policy_synch_policy_name):
+                    pol_href = uploaded_policy["href"]
+                    print "Deleting: uploaded policy, {} (href: {})".format(uploaded_policy["name"],pol_href)
+                    del_resp = requests.delete(gov_endpoint+pol_href, headers=gov_headers)
+            except:
+                print "Problem deleting policy"
+    except:
+        # no-op
+        print "No uploaded policies found."
+    
+    # Now that we have a clean slate, upload and apply the latest version of the policy synch template.
     print "Uploading Policy Synchronization Policy to account, {}.".format(account_id) 
-    gov_base_url = "https://{}/api/governance/projects/{}".format(gov_host,account_id)
     upload_response = requests.post(gov_base_url+"/policy_templates", headers=gov_headers, json=upload_body) 
     upload_response_status_code = upload_response.status_code
     if upload_response_status_code != 200:
         print "Account {}: Policy Sync policy failed to upload. Error Code: {}.".format(account_id, upload_response.status_code)
         if upload_response_status_code == 409:
             print "This is likely a policy template conflict error."
-            print "Check if the \"Policy Template Synchronization Policy Template\" policy has already been uplaoded."
-        print "Error Response Body: {}".format(upload_response.text)
-        did_not_complete = True
+            print "This should not have happened since the policy synch template should have been removed above."
+            print "Check if the \""+policy_synch_policy_name+"\" policy has already been uplaoded."
+        else:
+            print "Error Response Body: {}".format(upload_response.text)
+            did_not_complete = True
     else:
         # Get the info for the uploaded template
         upload_response_json = json.loads(upload_response.text)
