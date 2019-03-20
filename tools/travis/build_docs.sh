2be7ee128f2af68be1e3e0c56696fdc990d9a391
srpomeroy <sean.pomeroy@rightscale.com>
2019-03-20 14:25:27 -0400

    initial commit

---
+++ tools/travis/build_docs.sh
@@ -0,0 +1,12 @@
+body='{
+  "request": {
+  "branch":"master"
+}}'
+
+curl -s -X POST \
+   -H "Content-Type: application/json" \
+   -H "Accept: application/json" \
+   -H "Travis-API-Version: 3" \
+   -H "Authorization: token ${process.env.TRAVIS_API_TOKEN}" \
+   -d "$body" \
+   https://api.travis-ci.com/repo/rightscale%2Fdocs/requests
