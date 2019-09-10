00e1639bff6dd6d8d1d1355610855907030df863
Sean Pomeroy <sean.pomeroy@gmail.com>
2019-09-10 11:15:39 -0400

    move to azure tree

--- cost/aws/azure-sql-utilization/test.bat
+++
@@ -1,2 +0,0 @@
-
-fpt run azure_databases_utilization.pt param_avg_cpu=50 param_azure_sub=8beb7791-9302-4ae4-97b4-afd482aadc59 param_azure_tenant_id=09b8fec1-4b8d-48dd-8afa-5c1a775ea0f2 param_email="krasmussen@flexera.com" param_exclusion_tag_key=""
