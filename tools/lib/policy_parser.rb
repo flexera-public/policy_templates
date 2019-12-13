aa734a40cd65c8eb3939df1b10d25b828ef4b843
Curt Wilhelm <1047804+cdwilhelm@users.noreply.github.com>
2019-12-13 14:22:08 -0700

    POL-163 read info field (#420)
    * read info field
    
    * tuning
    
    * validating the version and provider
    
    * test file
    
    * test file
    
    * test missing provider
    
    * revert changes
    
    * update danger
    
    * revert changes
    
    * revert changes
    
    * formating
    
    * test file
    
    * fix
    
    * add policy parser
    
    * use policyparser to read fields
    
    * fixing errors when values are nil
    
    * adding check for publish key
    
    * remove test file
    
    * use require_relative
    
    * use require_relative
    check version in long_description
    
    * test file
    
    * remove test file

---
+++ tools/lib/policy_parser.rb
@@ -0,0 +1,42 @@
+class PolicyParser
+  DEFINITION_REGEX = /^[[:blank:]]*define[[:blank:]]*([\w_\.]+)[[:blank:]]*\([@$\w _,]*\)[[:blank:]]*.*do/.freeze
+
+  attr_reader :parsed_info,  :parsed_category, :parsed_name, :parsed_severity, :parsed_long_description, :parsed_short_description
+
+  def parse(file)
+    source = File.read(file)
+
+    # we assume that all of the metadata is before any declarations
+    # especially RCL defines which are not parsable as Ruby
+    source = source.split(DEFINITION_REGEX, 2).first
+    instance_eval(source, file)
+  end
+
+  def info(**info)
+    @parsed_info = info
+  end
+
+  def category(category)
+    @parsed_category = category
+  end
+
+  def name(name)
+    @parsed_name = name
+  end
+  
+  def severity(severity)
+    @parsed_severity = severity
+  end
+
+  def long_description(long_description)
+    @parsed_long_description = long_description
+  end
+
+  def short_description(short_description)
+    @parsed_short_description = short_description
+  end
+
+  def method_missing(symbol, *args, &block)
+    # do nothing
+  end
+end
