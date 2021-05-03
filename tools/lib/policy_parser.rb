b0bab9cdf0ecc2636427ab5114b3d83935ca3dd9
Sean Pomeroy <sean.pomeroy@gmail.com>
2021-05-03 10:27:44 -0400

    add parsed_default_frequency to attr_reader

--- tools/lib/policy_parser.rb
+++ tools/lib/policy_parser.rb
@@ -1,7 +1,7 @@
 class PolicyParser
   DEFINITION_REGEX = /^[[:blank:]]*define[[:blank:]]*([\w_\.]+)[[:blank:]]*\([@$\w _,]*\)[[:blank:]]*.*do/.freeze
 
-  attr_reader :parsed_info,  :parsed_category, :parsed_name, :parsed_severity, :parsed_long_description, :parsed_short_description
+  attr_reader :parsed_info,  :parsed_category, :parsed_name, :parsed_severity, :parsed_long_description, :parsed_short_description, :parsed_default_frequency
 
   def parse(file)
     source = File.read(file)
