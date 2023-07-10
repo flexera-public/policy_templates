910c9432105ddefa8302543bf65f4f97228cc501
Bryan Karaffa <bryankaraffa@gmail.com>
2023-07-10 16:09:26 +0000

    test: with debug logging

--- tools/meta_parent_policy_compiler/meta_parent_policy_compiler.rb
+++ tools/meta_parent_policy_compiler/meta_parent_policy_compiler.rb
@@ -172,8 +172,11 @@ def compile_meta_parent_policy(file_path)
   # This would only work if the pt file is located under the `policy_templates` repo directory
   # If it is not, then the URL will be incorrect
   pt_path_expanded = File.expand_path(file_path) # Get full path to the pt file provided
+  print("pt_path_expanded: #{pt_path_expanded}\n")
   pt_path_repo_file = pt_path_expanded.split("policy_templates")[1] # Get the path to the pt file relative to the policy_templates directory
+  print("pt_path_repo_file: #{pt_path_repo_file}\n")
   pt_path_repo_dir = pt_path_repo_file.split("/")[0..-2].join("/") # Get the path to the directory containing the child pt file
+  print("pt_path_repo_dir: #{pt_path_repo_dir}\n")
   github_url = "https://github.com/flexera-public/policy_templates/tree/master#{pt_path_repo_dir}" # Build the github URL
   output_pt = output_pt.gsub("__PLACEHOLDER_FOR_CHILD_POLICY_GITHUB_URL__", github_url) # Replace the placeholder with the github URL
   # Build a list of parameter block strings for the output pt
