e80e2243eb537c382c09cff861048125c4c0e6c9
nia-vf1 <92175447+nia-vf1@users.noreply.github.com>
2023-07-25 12:22:53 +0100

    Update meta_parent_policy_compiler.rb
    Add Lambda Functions With High Error Rate policy template to default templates list

--- tools/meta_parent_policy_compiler/meta_parent_policy_compiler.rb
+++ tools/meta_parent_policy_compiler/meta_parent_policy_compiler.rb
@@ -15,6 +15,7 @@ default_child_policy_template_files = [
   "../../cost/aws/unused_rds/unused_rds.pt",
   "../../cost/aws/unused_volumes/aws_delete_unused_volumes.pt",
   "../../compliance/aws/long_stopped_instances/aws_long_stopped_instances.pt",
+  "../../operational/aws/lambda_functions_with_high_error_rate/lambda_functions_with_high_error_rate.pt",
   # Azure Policy Templates
   "../../cost/azure/idle_compute_instances/azure_idle_compute_instances.pt",
   "../../cost/azure/old_snapshots/azure_delete_old_snapshots.pt",
