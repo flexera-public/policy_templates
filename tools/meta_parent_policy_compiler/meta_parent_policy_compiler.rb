c7ea6c68b361b2ca6976ced82b1a5fb36d7ee266
Shawn Huckabay <shuckabay@flexera.com>
2023-07-28 08:43:24 -0500

    POL-893 FlexNet Meta Policies (#1380)
    * added to compilter
    
    * added to compilter
    
    * updates
    
    * changelog
    
    * fix: exclude meta_parent.pt as new policy tempalte
    
    * updated to use dimension filtering
    
    ---------
    
    Co-authored-by: Bryan Karaffa <BKaraffa@flexera.com>

--- tools/meta_parent_policy_compiler/meta_parent_policy_compiler.rb
+++ tools/meta_parent_policy_compiler/meta_parent_policy_compiler.rb
@@ -6,6 +6,7 @@ require "json"
 # More info at https://github.com/flexera-public/policy_templates/blob/master/README_META_POLICIES.md
 default_child_policy_template_files = [
   # AWS Policy Templates
+  "../../compliance/aws/instances_without_fnm_agent/aws_instances_not_running_flexnet_inventory_agent.pt",
   "../../cost/aws/idle_compute_instances/idle_compute_instances.pt",
   "../../cost/aws/object_storage_optimization/aws_object_storage_optimization.pt",
   "../../cost/aws/old_snapshots/aws_delete_old_snapshots.pt",
@@ -18,6 +19,7 @@ default_child_policy_template_files = [
   "../../operational/aws/lambda_functions_with_high_error_rate/lambda_functions_with_high_error_rate.pt",
   "../../security/aws/rds_publicly_accessible/aws_publicly_accessible_rds_instances.pt",
   # Azure Policy Templates
+  "../../compliance/azure/instances_without_fnm_agent/azure_instances_not_running_flexnet_inventory_agent.pt",
   "../../cost/azure/idle_compute_instances/azure_idle_compute_instances.pt",
   "../../cost/azure/old_snapshots/azure_delete_old_snapshots.pt",
   "../../cost/azure/rightsize_compute_instances/azure_compute_rightsizing.pt",
