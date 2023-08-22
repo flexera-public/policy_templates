3e678be8646a0b293b0f7f4a12b341894527b1e1
Nia Velinor-Fraser <NVelinorFraser@flexera.com>
2023-08-22 11:35:54 +0100

    updated rb script

--- tools/policy_template_release_generation/generate_policy_release_notification_contents.rb
+++ tools/policy_template_release_generation/generate_policy_release_notification_contents.rb
@@ -16,23 +16,35 @@ class PolicyTemplate
   end
 end
 
+# Manually set branches here (for now)
+base_branch = 'master'
+head_branch = 'POL-847-policy-automated-updates-on-releases'
+
 # Get a list of added and modified files
-added_files = `git diff --name-only --diff-filter=A HEAD@{1} HEAD`.split("\n")
-modified_files = `git diff --name-only --diff-filter=M HEAD@{1} HEAD`.split("\n")
+
+#`ENV['DANGER_GITHUB_PR_BASE_SA'] + '...' + 'ENV['DANGER_GITHUB_PR_HEAD_SHA']
+#added_files = `git diff --name-only --diff-filter=A HEAD@{1} HEAD`.split("\n")
+#modified_files = `git diff --name-only --diff-filter=M HEAD@{1} HEAD`.split("\n")
+
+added_files = `git diff --name-only #{base_branch} #{head_branch} --diff-filter=A`.split("\n")
+modified_files = `git diff --name-only #{base_branch} #{head_branch} --diff-filter=M`.split("\n")
 puts "These are the Added Files: #{added_files}"
 puts "These are the Modified Files: #{modified_files}"
+changed_files = (added_files + modified_files).uniq
 
-# # Initialize arrays to store Changelog objects and Policy Template objects
-# changelogs = []
-# policy_templates = []
+# Initialize arrays to store Changelog objects and Policy Template objects
+changelogs = []
+policy_templates = []
 
-# # Process Changelog files
-# changelog_files.each do |changelog_file|
-#   changelog_content = File.read(changelog_file)
-#   version = changelog_content.match(/^##\s*v([\d.]+)/)&.captures&.first
-#   changes = changelog_content.scan(/^- (.+)/).flatten
-#   changelogs << Changelog.new(version, changes) if version && !changes.empty?
-# end
+# Process Changelog files
+changelog_files.each do |changelog_file|
+  changelog_content = File.read(changelog_file)
+  version = changelog_content.match(/^##\s*v([\d.]+)/)&.captures&.first
+  changes = changelog_content.scan(/^- (.+)/).flatten
+  changelogs << Changelog.new(version, changes) if version && !changes.empty?
+end
+
+puts "This is the list of Changelogs"
 
 # # Process Policy Template files
 # policy_template_files.each do |pt_file|
