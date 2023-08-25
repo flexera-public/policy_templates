c4de9d3c0980a21ce680d4d9adf84d04d9898ac7
Nia Velinor-Fraser <NVelinorFraser@flexera.com>
2023-08-25 10:47:28 +0100

    Updated workflow yaml - testing webhook 4

--- tools/policy_template_release_generation/generate_policy_release_notification_contents.rb
+++ tools/policy_template_release_generation/generate_policy_release_notification_contents.rb
@@ -20,7 +20,6 @@ end
 # Manually set branches here (for now)
 base_branch = 'origin/master'
 head_branch = 'POL-847-policy-automated-updates-on-releases'
-puts "Base branch #{base_branch}"
 
 # Get a list of added and modified files
 
@@ -30,7 +29,7 @@ puts "Base branch #{base_branch}"
 # changed_files = (added_files + modified_files).uniq
 
 changed_files = `git diff --name-only #{base_branch} #{head_branch}`.split("\n")
-puts "These are the Modified Files: #{changed_files}"
+# puts "These are the Modified Files: #{changed_files}"
 
 
 # Initialize arrays to store Changelog objects and Policy Template objects
@@ -63,18 +62,33 @@ changed_files.each do |file|
   end
 end
 
-puts "This is the list of Changelogs: #{changelogs}"
-puts "This is the list of Policy Templates: #{policy_templates}"
+# puts "This is the list of Changelogs: #{changelogs}"
+# puts "This is the list of Policy Templates: #{policy_templates}"
+
+# Create Notification Content Array
+notification_content_array = []
 
 # Match Changelog entries with Policy Templates based on paths
+# and then push Changelog contents to Notification Content Array defined above
 changelogs.each do |changelog|
   matching_template = policy_templates.find { |template| changelog.path.include?(File.dirname(template.path)) }
   if matching_template
-    puts "Changelog for Policy Template '#{matching_template.name}':"
-    puts "Updated Template Version '#{changelog.version}'"
-    puts changelog.changes
-    puts "Policy Template File Path: #{matching_template.path}"
-    puts "\n"
+    # puts "Changelog for Policy Template '#{matching_template.name}':"
+    # puts "Updated Template Version '#{changelog.version}'"
+    # puts changelog.changes
+    # puts "Policy Template File Path: #{matching_template.path}"
+    # puts "\n"
+
+    notification_content_json = {
+      activityTitle: matching_template.name,
+      activitySubtitle: "Version: #{changelog.version}",
+      facts: [{
+        name: "Updates",
+        value: changelog.changes
+      }]
+    }
+
+    notification_content_array << notification_content_json
 
     # # Store Changelog content in Step Output
     # puts "::set-output name=changelog_content::#{changelog.changes.join}"
@@ -82,3 +96,6 @@ changelogs.each do |changelog|
     puts "No matching Policy Template found for Changelog Version #{changelog.version}"
   end
 end
+
+#Output Notification Content as a JSON string
+puts changelog_json_array.to_json
