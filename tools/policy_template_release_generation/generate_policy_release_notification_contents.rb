d8624f461931fbeebfeb469fce6bc5dd27f8b7df
nia-vf1 <92175447+nia-vf1@users.noreply.github.com>
2023-08-31 15:12:55 +0100

    POL-847 Policy Automated Updates On Releases (Part 1) (#1440)
    * add draft notification generation script
    
    * some text changes
    
    * updated rb script
    
    * updated rb script
    
    * updated rb script
    
    * updated rb script
    
    * updated rb script
    
    * revert changes
    
    * add version number update
    
    * added some more test changes
    
    * updated ruby script for notifications workflow
    
    * added yaml file for github actions workflow
    
    * added option to manually trigger workflow to workflow yaml
    
    * corrected path to rb script
    
    * Test
    
    * Updated workflow yaml - testing webhook
    
    * Updated workflow yaml - testing webhook 2
    
    * Updated workflow yaml - testing webhook 3
    
    * Updated workflow yaml - testing webhook 4
    
    * Updated workflow yaml - testing webhook 4
    
    * Updated workflow yaml - testing webhook 4
    
    * Test change to multiple policy templates
    
    * Updated workflow yaml - testing webhook 4
    
    * Updated workflow yaml - testing webhook 4
    
    * Updated workflow yaml - testing webhook 5
    
    * Updated workflow yaml - test backtick fix
    
    * Updated workflow yaml - test backtick fix 2
    
    * Updated workflow yaml - test backtick fix 2
    
    * Updated workflow yaml - test backtick fix 2
    
    * Updated workflow yaml - test new line fix
    
    * Updated workflow yaml - test list in notification
    
    * Updated workflow yaml - test code formatting in html
    
    * Updated workflow yaml - test Activity header formatting in HTML
    
    * Updated workflow yaml - test css font size for Activity Header
    
    * Updated ruby script - test on master branch changes
    
    * Updated ruby script - test on master branch changes 2
    
    * Updated ruby script - test on master branch changes 3
    
    * Updated ruby script - test on master branch changes 3 - remove puts
    
    * Updated ruby script - test empty array results in no notification
    
    * Updated yaml workflow - test dispatch on master branch
    
    * Update generate-policy-release-notifications.yaml
    
    Added master branch push to workflow trigger
    
    * Update generate-policy-release-notifications.yaml
    
    remove POL-847 branch from manual trigger
    
    * test newest policy catalog update
    
    * test changelog content
    
    * test workflow with master branch most recent updates
    
    * remove test/dummy data from changelog/pt files
    
    * testing most recent updates to master
    
    * tidied up rb script
    
    * tidied up yaml script

---
+++ tools/policy_template_release_generation/generate_policy_release_notification_contents.rb
@@ -0,0 +1,92 @@
+require 'json'
+
+class Changelog
+  attr_accessor :path, :version, :changes
+
+  def initialize(path, version, changes)
+    @path = path
+    @version = version
+    @changes = changes
+  end
+end
+
+class PolicyTemplate
+  attr_accessor :name, :path
+
+  def initialize(name, path)
+    @name = name
+    @path = path
+  end
+end
+
+# Get a list of added and modified files
+added_files = `git diff --name-only --diff-filter=A origin/master~1..origin/master`.split("\n")
+modified_files = `git diff --name-only --diff-filter=M origin/master~1..origin/master`.split("\n")
+changed_files = (added_files + modified_files).uniq
+
+# Initialize arrays to store Changelog objects and Policy Template objects
+changelogs = []
+policy_templates = []
+
+# Process Changelog files
+changed_files.each do |file|
+  next unless file.match?(/CHANGELOG\.md$/) || file.match?(/\.pt$/) #Consider pulling README as well
+
+  if file.match?(/CHANGELOG\.md$/) 
+    changelog_content = File.read(file)
+    version = changelog_content.match(/^##\s*v([\d.]+)/)&.captures&.first
+    changes = []
+
+    if version && !changelog_content.empty?
+      # Capture changes for the most recent version only
+      latest_version_changes = changelog_content.scan(/^##\s*v#{version}[\s\S]*?(?=(?:^##\s*v\d+)|\z)/m)
+      latest_version_changes.each do |version_changes|
+        changes.concat(version_changes.scan(/^- (.+)/).flatten)
+      end
+
+      changelogs << Changelog.new(file, version, changes) if version && !changes.empty?
+    end
+  else
+    # Capture policy template names
+    pt_content = File.read(file)
+    name = pt_content.match(/name "([^"]+)"/)&.captures&.first
+    policy_templates << PolicyTemplate.new(name, file) if name
+  end
+end
+
+# Create Notification Content Array
+all_notification_content_array = []
+
+# Match Changelog entries with Policy Templates based on paths
+# and then push Changelog contents to Notification Content Array defined above
+changelogs.each do |changelog|
+  matching_template = policy_templates.find { |template| changelog.path.include?(File.dirname(template.path)) }
+  if matching_template
+
+    # Format HTML for Notification API call
+    # Replace backticks around words with HTML code tags
+    formatted_changes = changelog.changes.map do |change|
+      change.gsub(/`([^`]+)`/, '<code>\1</code>')
+    end
+
+    formatted_changes_html = formatted_changes.map { |change| "<li>#{change}</li>"}.join('')
+
+    notification_content_json = {
+      activityTitle: "<h2 style='font-size: 18px;'>#{matching_template.name}</h2>",
+      facts: [{
+        name: "Template Version",
+        value: changelog.version
+      },
+      {
+        name: "Policy Updates",
+        value: "<ul>#{formatted_changes_html}</ul>"
+      }]
+    }
+
+    all_notification_content_array << notification_content_json
+  end
+end
+
+# Output Notification Content as a JSON string to be used directly in YAML workflow file
+all_notification_content = JSON.generate(all_notification_content_array).gsub('"', '\\"')
+puts all_notification_content
