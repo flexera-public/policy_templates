4305f4f7dd1cb96530f8fc00b7cf9664397f4eba
Shawn Huckabay <shuckabay@flexera.com>
2024-01-22 14:06:32 -0600

    good (#1768)

--- tools/policy_master_permission_generation/generate_policy_master_permissions.rb
+++ tools/policy_master_permission_generation/generate_policy_master_permissions.rb
@@ -23,6 +23,7 @@ pt_files = [
   "./cost/azure/old_snapshots/azure_delete_old_snapshots.pt",
   "./cost/azure/unused_ip_addresses/azure_unused_ip_addresses.pt",
   "./cost/azure/reserved_instances/recommendations/azure_reserved_instance_recommendations.pt",
+  "./operational/azure/tag_cardinality/azure_tag_cardinality.pt"
 ]
 
 
