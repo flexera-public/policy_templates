9816edb5ebb3eba105f29091ecd0799c6e1817d6
Shawn Huckabay <shuckabay@flexera.com>
2024-01-22 14:22:42 -0600

    rightsize (#1771)

--- tools/policy_master_permission_generation/generate_policy_master_permissions.rb
+++ tools/policy_master_permission_generation/generate_policy_master_permissions.rb
@@ -25,6 +25,8 @@ pt_files = [
   "./cost/azure/savings_plan/recommendations/azure_savings_plan_recommendations.pt",
   "./cost/azure/reserved_instances/recommendations/azure_reserved_instance_recommendations.pt",
   "./operational/azure/tag_cardinality/azure_tag_cardinality.pt",
+  # Google
+  "./cost/google/rightsize_vm_recommendations/google_rightsize_vm_recommendations.pt"
 ]
 
 
