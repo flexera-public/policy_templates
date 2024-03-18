c89f1399e4ce97089dbf54cbca86c1ce2ff17205
Shawn Huckabay <shuckabay@flexera.com>
2024-03-18 14:22:36 -0500

    update

--- .dangerfile/github_tests.rb
+++ .dangerfile/github_tests.rb
@@ -16,7 +16,6 @@ def github_pr_bad_title?(github)
     fail_message = "**Github Pull Request**\nPull Request has improper title. Title should always begin with the JIRA ticket id, followed by a description, like in the following examples:\n\n"
     fail_message += "POL-123 Add New Feature\n"
     fail_message += "POL-1000 Fixed Bug\n\n"
-    fail_message += github.pr_labels
   end
 
   return fail_message.strip if !fail_message.empty?
@@ -38,6 +37,16 @@ end
 def github_pr_missing_labels?(github)
   fail_message = ""
   fail_message = "**Github Pull Request**\nPull Request is missing labels. Please add labels to this Pull Request." if github.pr_labels.empty?
+
+  return fail_message.strip if !fail_message.empty?
+  return false
+end
+
+### Missing Github PR Ready label
+# Verify that the pull request has ready for review label
+def github_pr_missing_ready_label?(github)
+  fail_message = ""
+  fail_message = "**Github Pull Request**\nPull Request is missing `READY-FOR-REVIEW` label. Please add this label if this Pull Request is ready for review." if !github.pr_labels.include?("READY-FOR-REVIEW")
 
   return fail_message.strip if !fail_message.empty?
   return false
