3bbd5c8d3ef6ec213840341d3ee0bccffe86b134
Shawn Huckabay <shuckabay@flexera.com>
2024-05-06 15:56:41 -0500

    update

--- tools/meta_parent_policy_compiler/meta_parent_policy_compiler.rb
+++ tools/meta_parent_policy_compiler/meta_parent_policy_compiler.rb
@@ -76,6 +76,7 @@ default_child_policy_template_files = [
   "../../cost/google/cloud_sql_idle_instance_recommendations/google_sql_idle_instance_recommendations.pt",
   "../../cost/google/idle_ip_address_recommendations/google_idle_ip_address_recommendations.pt",
   "../../cost/google/idle_persistent_disk_recommendations/google_idle_persistent_disk_recommendations.pt",
+  "../../cost/google/object_storage_optimization/google_object_storage_optimization.pt",
   "../../cost/google/recommender/recommender.pt",
   "../../cost/google/rightsize_vm_recommendations/google_rightsize_vm_recommendations.pt",
   "../../cost/google/schedule_instance/google_schedule_instance.pt",
