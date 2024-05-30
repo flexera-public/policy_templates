aa8bf5b105455edae7bc597bb510b98a94b7725f
Ramyad <62895311+ramyadmz@users.noreply.github.com>
2024-05-30 12:04:55 -0700

    Applying reviews : add a second \n

--- .dangerfile/readme_tests.rb
+++ .dangerfile/readme_tests.rb
@@ -423,7 +423,7 @@ def readme_invalid_credentials?(file)
 
           if !line.split("  - ")[1].match?(flexera_perm_tester)
             fail_message += "Line #{line_number.to_s}: Flexera permission list item formatted incorrectly. Please make sure all list items are formatted like the following examples:\n\n"
-            fail_message += "```  - `billing_center_viewer`*```\n"
+            fail_message += "```  - `billing_center_viewer`*```\n\n"
           end
         end
       end
@@ -433,7 +433,7 @@ def readme_invalid_credentials?(file)
 
     if permission_list_found == 0
       fail_message += "Flexera permission list missing or formatted incorrectly. Please ensure there is a list of permissions beneath the Flexera permission statement. Each list item should begin with [space][space][hyphen][space] like so:\n\n"
-      fail_message += "```  - `billing_center_viewer`*```\n"
+      fail_message += "```  - `billing_center_viewer`*```\n\n"
     end
 
     if asterix_found == 1
