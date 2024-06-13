58300535c7111d4e16c0a8cc91864346a58cf79e
Shawn Huckabay <shuckabay@flexera.com>
2024-06-13 13:53:24 -0500

    update

--- tools/cloud_data/aws/aws_ec2_instance_types.py
+++ tools/cloud_data/aws/aws_ec2_instance_types.py
@@ -6,30 +6,27 @@ import boto3
 from botocore.auth import SigV4Auth
 from botocore.awsrequest import AWSRequest
 
-def calculate_normalized_units(instance_type_info):
-  vcpus = instance_type_info['VCpuInfo']['DefaultVCpus']
-  memory = instance_type_info['MemoryInfo']['SizeInMiB']
-  # Example multipliers for CPU and Memory
-  cpu_multiplier = 1
-  memory_multiplier = 0.1
-  normalized_units = (vcpus * cpu_multiplier) + (memory * memory_multiplier)
-  return normalized_units
-
+region_filename = f'data/aws/regions.json'
 output_filename = f'data/aws/aws_ec2_instance_types.json'
+
+with open(region_filename, 'r') as file:
+  region_list = json.load(file)
 
 print("Gathering data from AWS API...")
 
 data = {}
+data["instanceTypes"] = {}
 
 # Create a session
+region = 'us-east-1'
 session = boto3.Session()
 
 # Create a SigV4Auth object
 credentials = session.get_credentials()
-auth = SigV4Auth(credentials, 'ec2', 'us-east-1')
+auth = SigV4Auth(credentials, 'ec2', region)
 
 # Prepare the request
-request = AWSRequest(method='GET', url='https://ec2.us-east-1.amazonaws.com?Action=DescribeInstanceTypes&Version=2016-11-15', params={'Action': 'DescribeInstanceTypes', 'Version': '2016-11-15'})
+request = AWSRequest(method='GET', url='https://ec2.' + region + '.amazonaws.com?Action=DescribeInstanceTypes&Version=2016-11-15')
 auth.add_auth(request)
 
 # Send the request
@@ -40,7 +37,7 @@ if response.status_code == 200:
   instance_list = xmltodict.parse(response.content)["DescribeInstanceTypesResponse"]["instanceTypeSet"]["item"]
 
   for item in instance_list:
-    data[item["instanceType"]] = item
+    data["instanceTypes"][item["instanceType"]] = item
 
 print("Writing final output to file...")
 
