dc054ed02140a13afdd45cd8c64f44f994ab70d1
Shawn Huckabay <shuckabay@flexera.com>
2025-04-10 15:27:02 -0400

    update

--- tools/cloud_data/google/google_compute_instance_types.py
+++ tools/cloud_data/google/google_compute_instance_types.py
@@ -39,48 +39,71 @@ def list_all_machine_types():
 
     return machine_types_list
 
-if __name__ == '__main__':
-    machine_types = list_all_machine_types()
+machine_types = list_all_machine_types()
 
-    data = {}
+data = {}
 
-    for item in machine_types:
-        name = item.get("name", "None")
-        zone = item.get("zone", "None")
+for item in machine_types:
+    name = item.get("name", "None")
+    zone = item.get("zone", "None")
 
-        if name in data:
-            if zone != "None" and zone not in data[name]["zones"]:
-                data[name]["zones"].append(zone)
-        else:
-            data[name] = {
-                "name": name,
-                "description": item.get("description", "None"),
-                "zones": [ zone ],
-                "specs": {
-                    "guestCpus": item.get("guestCpus", "None"),
-                    "memoryMb": item.get("memoryMb", "None"),
-                    "imageSpaceGb": item.get("imageSpaceGb", "None"),
-                    "maximumPersistentDisks": item.get("maximumPersistentDisks", "None"),
-                    "maximumPersistentDisksSizeGb": item.get("maximumPersistentDisksSizeGb", "None"),
-                    "isSharedCpu": item.get("isSharedCpu", "None"),
-                    "architecture": item.get("architecture", "None"),
-                    "accelerators": item.get("accelerators", "None"),
-                    "scratchDisks": item.get("scratchDisks", "None")
-                }
+    if len(name.split('-')) > 1:
+        family = name.split('-')[0]
+        variant = "-".join(name.split('-')[1:])
+
+    else:
+        family = name
+        variant = "None"
+
+    if name in data:
+        if zone != "None" and zone not in data[name]["zones"]:
+            data[name]["zones"].append(zone)
+    else:
+        memoryMb = item.get("memoryMb", "None")
+        memoryGb = "None"
+
+        if memoryMb != "None":
+            memoryGb = memoryMb / 1024
+
+        maximumPersistentDisksSizeGb = item.get("maximumPersistentDisksSizeGb", "None")
+
+        if maximumPersistentDisksSizeGb != "None":
+            maximumPersistentDisksSizeGb = int(maximumPersistentDisksSizeGb)
+
+        data[name] = {
+            "name": name,
+            "family": family,
+            "variant": variant,
+            "description": item.get("description", "None"),
+            "zones": [ zone ],
+            "specs": {
+                "guestCpus": item.get("guestCpus", "None"),
+                "memoryMb": memoryMb,
+                "memoryGb": memoryGb,
+                "imageSpaceGb": item.get("imageSpaceGb", "None"),
+                "maximumPersistentDisks": item.get("maximumPersistentDisks", "None"),
+                "maximumPersistentDisksSizeGb": maximumPersistentDisksSizeGb,
+                "isSharedCpu": item.get("isSharedCpu", "None"),
+                "architecture": item.get("architecture", "None"),
+                "accelerators": item.get("accelerators", "None"),
+                "scratchDisks": item.get("scratchDisks", "None")
             }
+        }
 
-    print("Writing final output to file...")
+final_data = list(data.values())
 
-    with open(output_filename, "w") as type_file:
-        type_file.write(
-            json.dumps(data, sort_keys=True, indent=2)
-                .replace(': ""', ': null')
-                .replace(': "none"', ': null')
-                .replace(': "None"', ': null')
-                .replace(': "true"', ': true')
-                .replace(': "True"', ': true')
-                .replace(': "false"', ': false')
-                .replace(': "False"', ': false')
-        )
+print("Writing final output to file...")
 
-    print("DONE!")
+with open(output_filename, "w") as type_file:
+    type_file.write(
+        json.dumps(final_data, sort_keys=False, indent=2)
+            .replace(': ""', ': null')
+            .replace(': "none"', ': null')
+            .replace(': "None"', ': null')
+            .replace(': "true"', ': true')
+            .replace(': "True"', ': true')
+            .replace(': "false"', ': false')
+            .replace(': "False"', ': false')
+    )
+
+print("DONE!")
